7f41c9e67baff175a3d485aac5e8e5b3e0952569
Hubert <hubert.wachowski@goeuropa.eu>
2023-04-26 17:45:36 +0200

    add log to api

--- transitclockApi/src/main/java/org/transitclock/api/rootResources/TransitimeApi.java
+++ transitclockApi/src/main/java/org/transitclock/api/rootResources/TransitimeApi.java
@@ -24,6 +24,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
