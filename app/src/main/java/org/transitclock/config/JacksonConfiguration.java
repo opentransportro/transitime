06ac00d1a2ec331cebc8db885d2f8581cc6e6111
Vlad Vesa <vlad.vesa@outlook.com>
2024-03-25 07:19:41 +0200

    Do not serialize null fields

--- app/src/main/java/org/transitclock/config/JacksonConfiguration.java
+++ app/src/main/java/org/transitclock/config/JacksonConfiguration.java
@@ -1,8 +1,10 @@
 package org.transitclock.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
@@ -10,6 +12,10 @@ import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 @Configuration
 public class JacksonConfiguration {
 
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+    }
     /**
      * Support for Java date and time API.
      * @return the corresponding Jackson module.
